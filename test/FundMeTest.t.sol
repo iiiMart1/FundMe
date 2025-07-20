f9585472fd820e8d67028cefd057cc0487f6099d
iiiMart1 <work.mohammedimarwane@gmail.com>
2025-07-20 12:20:04 +0100

    refactoring tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,19 +3,22 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
-    function testMinimumDollarIsFive() public {
+    function testMinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
 
-    function testOwnerIsMsgSender() public {
+    function testOwnerIsMsgSender() public view {
         // fundMe.i_owner was set in setUp(), during deployment
         // At that time, the deploying address was address(this) (the test contract)
         console.log("i_owner (set during deployment):", fundMe.i_owner());
@@ -29,7 +32,7 @@ contract FundMeTest is Test {
         console.log("address(this):", address(this));
 
         // This will pass ✅ because we deployed FundMe from address(this)
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
 
         // This will fail ❌ because msg.sender ≠ address(this) in test functions
         // Uncommenting this line would make the test fail:
@@ -37,7 +40,7 @@ contract FundMeTest is Test {
         //Testing
     }
 
-    function testPriceFeedVersionAccurate() public {
+    function testPriceFeedVersionAccurate() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
     }
